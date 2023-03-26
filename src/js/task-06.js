73133e82372fe237aa037311845a1fd856a1462c
Maniyavel <116820894+Maniyavel@users.noreply.github.com>
2023-03-26 19:52:19 +0300

    add task6

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,2 +1,12 @@
 const input=document.querySelector('#validation-input');
 const inputLength=Number(input.getAttribute("data-length"));
+
+input.addEventListener("blur", event => {
+  if (event.target.value.length === inputLength) {
+    input.classList.add("valid");
+
+    
+  } else {
+    input.classList.add("invalid");
+  }
+});
