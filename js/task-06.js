0d08db8711a6f671d80a97dc648f70b232cf4b05
Maniyavel <116820894+Maniyavel@users.noreply.github.com>
2023-03-29 16:29:20 +0300

    fix task6

--- js/task-06.js
+++ js/task-06.js
@@ -4,7 +4,9 @@ const inputLength=Number(input.getAttribute("data-length"));
 input.addEventListener("blur", event => {
   if (event.target.value.length === inputLength) {
     input.classList.add("valid");
-
+    if (input.classList.contains('invalid')) {
+      input.classList.remove('invalid');
+  }
     
   } else {
     input.classList.add("invalid");
